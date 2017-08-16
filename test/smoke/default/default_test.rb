8465890833f44c9c3da448cfbe34ba78328d25ae
Alexander Oleynik <x103103@gmail.com>
2017-08-16 12:22:28 +0300

    add php app

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,10 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe command("mysql -h 127.0.0.1 -uroot -pfakerootpassword -D 4thcoffee -e 'describe customers;'") do
+  its('stdout') { should match(/id/) }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe command 'wget -qO- localhost' do
+  its('stdout') { should match(/Customers/) }
 end
