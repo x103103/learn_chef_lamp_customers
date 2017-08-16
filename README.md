f8901efdb27e31c3cdd24400df6e1e2b175a0c97
Alexander Oleynik <x103103@gmail.com>
2017-08-16 10:07:31 +0300

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# lamp_customers
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: lamp_customers
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
